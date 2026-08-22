133998c62569c0c303ce155b14748960b701c95a
Okabe Keigo <kampaku123456789@gmail.com>
2026-08-22 15:41:24 +0900

    .envの参照先をフロント・バックエンドで統一

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,6 +7,7 @@ import { LoginModule } from './login/login.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import jwtConfig from './auth/jwt.config';
 import dbConfig from './utils/db.config';
+import { resolve } from 'path';
 
 // ルートモジュール
 // ここからモジュールを読み込んでいくため、すべてのモジュールをここでインポートしておく
@@ -17,7 +18,8 @@ import dbConfig from './utils/db.config';
     // 環境変数をアプリケーション全体に提供する(NEST経由でprocess.envにアクセス可能)
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['../.env'],
+      // 実行ディレクトリに依存せず、モノレポルートの.envを読む
+      envFilePath: [resolve(__dirname, '../../.env')],
       // ConfigService.get('~')でアクセス可能
       load: [jwtConfig, dbConfig],
     }),
